5a70e1954a2e35fafcc083ccbbd83f2bfd915a28
Nayan Machhi <nayan.machhi@investisdigital.com>
2024-03-17 13:06:31 +0000

    new code updated now

---
+++ js/header.js
@@ -0,0 +1,9 @@
+function btndetails() {
+  document.getElementById("right-content-head").style.display = "block";
+  document.getElementById("right-content-head").style.transform = "translatex(0%)";
+  document.getElementById("right-content-head").style.transition = "0.5s";
+}
+
+function closeicon() {
+  document.getElementById("right-content-head").style.display = "none";
+}
