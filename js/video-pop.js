9b1759ad895343a73e89270abe6a918e1e389f12
Nayan Machhi <nayan.machhi@investisdigital.com>
2024-03-31 14:18:52 +0100

    pop video background bug fixed now

--- js/video-pop.js
+++ js/video-pop.js
@@ -2,14 +2,29 @@ function videobtn() {
   document.getElementById("videoid").style.display = "block";
 }
 
-function closevideo(){
- document.getElementById("videoid").style.display = "none";
+function closevideo() {
+  document.getElementById("videoid").style.display = "none";
+
+  videoStopper("videoid");
 }
 
-function videopopfeature(){
+function videopopfeature() {
   document.getElementById("videopop").style.display = "block";
 }
 
 function featurevideoclose() {
+  debugger;
   document.getElementById("videopop").style.display = "none";
+  videoStopper("videopop");
 }
+
+let videoStopper = function (id) {
+  debugger;
+  let containerElement = document.getElementById(id);
+  let iframe_tag = containerElement.querySelector("iframe");
+  if (iframe_tag) {
+    debugger;
+    let iframeSrc = iframe_tag.src;
+    iframe_tag.src = iframeSrc;
+  }
+};
